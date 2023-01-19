cb96da09ed88f05651d8766e06aafb7e51f43752
valhar <kharkov.valery@gmail.com>
2023-01-19 20:19:38 +0300

    В шаблоне index.html найдите тег с началом (Форма начинается с <form).....

--- lab_1/urls.py
+++ lab_1/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('common.urls')),
-    path('', include('test_app.urls'))
+    path('', include('test_app.urls')),
+    path('', include('login.urls'))
 ]
