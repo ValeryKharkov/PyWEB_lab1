caf69dbff5bc1c39fd75b353d8e5b4dbb3004e75
ValeryKharkov <kharkov.valery@gmail.com>
2023-01-19 16:46:09 +0300

    6. Теперь необходимо связать это представление с конкретным URL....

---
+++ common/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import CurrentDateView
+
+urlpatterns = [
+    path('datetime/', CurrentDateView.as_view()),
+]
--- common/views.py
+++ common/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
+from datetime import datetime
+
 
 # Create your views here.
+class CurrentDateView(View):
+    def get(self, request):
+        html = f'{datetime.now()}'
+        return HttpResponse(html)
