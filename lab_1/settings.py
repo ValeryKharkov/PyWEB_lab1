011d9132b3f0b5d9efca4f0813b3e2a0d61c5156
ValeryKharkov <kharkov.valery@gmail.com>
2023-01-20 12:43:31 +0300

    В pythonanywhere выбираем Consoles и Bash....

--- lab_1/settings.py
+++ lab_1/settings.py
@@ -9,28 +9,32 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
-from dotenv import load_dotenv  # TODO Не устанавливается пакет dotenv
+
+from dotenv import load_dotenv
 
 # Харьков: загрузка среды операционной системы из .env файла
-
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dgzpt6#a+hr_y&yg$a9yp!px)nu&zw=n&9t6ce6v313&$sz8!9'
+# SECRET_KEY = 'django-insecure-dgzpt6#a+hr_y&yg$a9yp!px)nu&zw=n&9t6ce6v313&$sz8!9' # Исходное значение
+
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True # Исходное значение
+DEBUG = os.getenv('DEBUG') == 'true'
 
-ALLOWED_HOSTS = []
 
+# ALLOWED_HOSTS = [] # Исходное значение
+ALLOWED_HOSTS = [host.strip() for host in os.getenv('ALLOWED_HOSTS').split(',')]
 
 # Application definition
 
@@ -75,7 +79,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'lab_1.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
@@ -85,7 +88,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -105,7 +107,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -116,7 +117,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
