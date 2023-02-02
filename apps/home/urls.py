e41ba3415b00fe73d91dfb32cb39967163dc17f9
valhar <kharkov.valery@gmail.com>
2023-02-02 20:55:57 +0300

    Практика №3. Пункт 37.

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from .views import IndexShopView
 
+app_name = 'home'
+
 urlpatterns = [
-   path('', IndexShopView.as_view()),
+   path('', IndexShopView.as_view(), name='index'),
 ]
 
