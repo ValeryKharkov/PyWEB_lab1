e38b2470773bfa4c244d397d68412b94059e3478
valhar <kharkov.valery@gmail.com>
2023-02-16 19:22:04 +0300

    Практика №4. Пункт 16

---
+++ apps/auth_shop/views.py
@@ -0,0 +1,22 @@
+from django.views import View
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+
+
+class Login(View):
+   def get(self, request):
+       return render(request, "auth_shop/index.html")
+
+   def post(self, request):
+       form = AuthenticationForm(data=request.POST)
+       if form.is_valid():
+           username = form.cleaned_data.get('username')
+           password = form.cleaned_data.get('password')
+           user = authenticate(username=username, password=password)
+           if user is not None:
+               login(request, user)
+               print(user)
+               return redirect('home:index')
+       return redirect('auth_shop:login')
+
