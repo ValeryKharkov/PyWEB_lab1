3f9031f119c1fa68aee5638bad112995f28124bc
ValeryKharkov <kharkov.valery@gmail.com>
2023-02-23 18:15:21 +0300

    Практика №4. пункт 36. Далее необходимо выполнить зачётное задание

--- apps/auth_shop/views.py
+++ apps/auth_shop/views.py
@@ -4,6 +4,7 @@ from .forms import CustomUserCreationForm
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from apps.cart_shop.models import Cart
 
 
 class Login(View):
@@ -34,7 +35,9 @@ class CreateUserView(View):
            email = form.cleaned_data.get('email')
            password = form.cleaned_data.get('password1')
            user = User.objects.create_user(username=username, email=email, password=password)
+           cart = Cart(user=user)
            user.save()
+           cart.save()
            login(request, user)
            return redirect('home:index')
        return render(request, "auth_shop/create_account.html", context={'errors': form.errors})
